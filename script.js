d2c7cfed8a334359ae8ea4f065db1a88575580d6
Rachit <154794339+bansalrachit19@users.noreply.github.com>
2024-08-21 17:38:02 +0530

    Update script.js

--- script.js
+++ script.js
@@ -114,10 +114,10 @@ function renderWeatherInfo(weatherInfo) {
     countryIcon.src = `https://flagcdn.com/144x108/${weatherInfo?.sys?.country.toLowerCase()}.png`;
     desc.innerText = weatherInfo?.weather?.[0]?.description;
     weatherIcon.src = `http://openweathermap.org/img/w/${weatherInfo?.weather?.[0]?.icon}.png`;
-    temp.innerText = weatherInfo?.main?.temp;
-    windspeed.innerText = weatherInfo?.wind?.speed;
-    humidity.innerText = weatherInfo?.main?.humidity;
-    cloudiness.innerText = weatherInfo?.clouds?.all;
+    temp.innerText = `${weatherInfo?.main?.temp} °C`;
+    windspeed.innerText = `${weatherInfo?.wind?.speed} m/sec `;
+    humidity.innerText = `${weatherInfo?.main?.humidity} % `;
+    cloudiness.innerText = `${weatherInfo?.clouds?.all} % `;
 }
 
 //agar coordinates already local storage par saved nahin hai, then pehle ham save karvayenge
